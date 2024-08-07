2d67996b2f488d2b66c4a2cfdcf8e893df1b581b
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-08-07 14:41:27 -0700

    Update zshift.swift

--- zshift/Sources/zshift/zshift.swift
+++ zshift/Sources/zshift/zshift.swift
@@ -3,34 +3,16 @@ import Figlet
 import Foundation
 
 @main
-struct Zshift: AsyncParsableCommand {
+struct ZShift: AsyncParsableCommand {
 
   static let configuration: CommandConfiguration = {
     return CommandConfiguration(
       abstract: "🖨️ | Hides and promotes zsh themes.",
       shouldDisplay: false,
-      subcommands: [Random.self, Like.self],
+      subcommands: [Random.self, Like.self, Exclude.self],
       defaultSubcommand: Random.self,
       helpNames: .shortAndLong)
   }()
-
-  /// Define constants for themes directory
-  static let themesDir = "~/.oh-my-zsh/themes/"
-
-  /// Default directory while Bundle loading is fixed.
-  static let defaultExcludedFile =
-    "~/Code/configs/zshift/Sources/zshift/Resources/excluded_zsh_themes.txt"
-
-  /// Default liked file while Bundle loading is fixed.
-  static let defaultLikedFile =
-    "~/Code/configs/zshift/Sources/zshift/Resources/liked_zsh_themes.txt"
-}
-
-struct Random: AsyncParsableCommand {
-
-  static var configuration = CommandConfiguration(
-    abstract: "Like a zsh theme",
-    helpNames: .shortAndLong)
 
   /// Function to expand "~" in file paths
   ///
@@ -42,6 +24,17 @@ struct Random: AsyncParsableCommand {
   static func expandTilde(in path: String) -> String {
     NSString(string: path).expandingTildeInPath
   }
+
+  /// Define constants for themes directory
+  static let themesDir = "~/.oh-my-zsh/themes/"
+
+  /// Default directory while Bundle loading is fixed.
+  static let defaultExcludedFile =
+    "~/Code/configs/zshift/Sources/zshift/Resources/excluded_zsh_themes.txt"
+
+  /// Default liked file while Bundle loading is fixed.
+  static let defaultLikedFile =
+    "~/Code/configs/zshift/Sources/zshift/Resources/liked_zsh_themes.txt"
 
   /// Load excluded themes from file, falling back to default resource if necessary
   /// NOTE: An fatal error if the file cannot be read.
@@ -76,89 +69,6 @@ struct Random: AsyncParsableCommand {
     return Set(excludedThemes.filter { !$0.isEmpty }).sorted()
   }
 
-  /// Get the list of available themes and exclude the ones specified in the file
-  static func getAvailableThemes(excludedThemes: [String]) -> [String] {
-    // Get the list of all available ZSH themes.
-    guard
-      let allThemes = try? FileManager.default.contentsOfDirectory(
-        atPath: Self.expandTilde(in: Zshift.themesDir))
-    else {
-      fatalError("Failed to list themes at \(Zshift.themesDir)")
-    }
-    // Filter out the bad themes.
-    return ["random"]
-      + allThemes.filter {
-        !excludedThemes.contains($0.replacingOccurrences(of: ".zsh-theme", with: ""))
-      }
-  }
-
-  /// Randomly select a theme from the list of available ones
-  static func getRandomTheme(from themes: [String]) -> String? {
-    themes.randomElement()?.components(separatedBy: "/").last?.replacingOccurrences(
-      of: ".zsh-theme", with: "")
-  }
-
-  /// Print out the path to the selected theme in zsh-compatible format
-  static func printSelectedTheme(_ theme: String) {
-    Figlet.say("Zshift - " + theme)
-    print("ZSH_THEME=\(theme)")
-  }
-
-  /// Read input
-  static func readInput() -> String {
-    print(
-      "Enter the path to your bad themes file (e.g., ~/excluded_zsh_themes.txt): ", terminator: "")
-    guard let excludedThemesPath = readLine() else {
-      fatalError("Failed to read file path")
-    }
-    return excludedThemesPath
-  }
-
-  func run() async throws {
-    /// For the script version just load the default
-    var excludedThemesPath = ""
-
-    // Maybe add as a resource?
-    if excludedThemesPath.count == 0 {
-      excludedThemesPath = Zshift.defaultExcludedFile
-    }
-
-    // Load excluded themes.
-    let likedThemes = Self.loadExcludedThemes(from: Zshift.defaultLikedFile)
-    // Load liked themes.
-    let excludedThemes = Self.loadExcludedThemes(from: excludedThemesPath)
-    // Filter out the seen themes.
-    var goodThemes = Self.getAvailableThemes(excludedThemes: excludedThemes + likedThemes)
-    if goodThemes.count == 0 {
-      goodThemes = likedThemes
-    }
-    // Choose a random good theme.
-    guard let randomTheme = Self.getRandomTheme(from: goodThemes) else {
-      fatalError("Random theme not there.")
-    }
-
-    Self.printSelectedTheme(randomTheme)
-  }
-}
-
-struct Like: AsyncParsableCommand {
-  static var configuration = CommandConfiguration(
-    abstract: "Like a zsh theme", helpNames: .shortAndLong)
-
-  @Argument(help: "The theme to like.")
-  var likedTheme: String
-
-  /// Function to expand "~" in file paths
-  ///
-  /// Alternate version
-  ///  guard let range = path.range(of: "~") else {
-  ///    return path
-  ///  }
-  /// return "\(NSHomeDirectory())\(path.replacingCharacters(in: range, with: ""))"
-  static func expandTilde(in path: String) -> String {
-    NSString(string: path).expandingTildeInPath
-  }
-
   /// Load excluded themes from file, falling back to default resource if necessary
   /// NOTE: An fatal error if the file cannot be read.
   /// - Parameter path The path to the file containing the list of excluded themes.
@@ -188,14 +98,13 @@ struct Like: AsyncParsableCommand {
       }
       contents = defaultContents
     }
-
-    let excludedThemes = contents.components(separatedBy: .newlines)
-    return Set(excludedThemes.filter { !$0.isEmpty }).sorted()
+    let likedThemes: [String] = contents.components(separatedBy: .newlines)
+    return Set(likedThemes.filter { !$0.isEmpty }).sorted()
   }
 
-  static func appendLikedTheme(_ theme: String, to path: String) throws {
-    let expandedPath = expandTilde(in: path)
-    let themeToAppend = theme + "\n"
+  static func append(theme: String, to path: String) throws {
+    let expandedPath: String = ZShift.expandTilde(in: path)
+    let themeToAppend: String = theme + "\n"
 
     if let fileHandle = FileHandle(forWritingAtPath: expandedPath) {
       defer { fileHandle.closeFile() }
@@ -206,20 +115,27 @@ struct Like: AsyncParsableCommand {
       try themeToAppend.write(toFile: expandedPath, atomically: true, encoding: .utf8)
     }
   }
+}
+
+struct Random: AsyncParsableCommand {
+  static var configuration = CommandConfiguration(
+    abstract: "Like a zsh theme",
+    helpNames: .shortAndLong)
 
   /// Get the list of available themes and exclude the ones specified in the file
   static func getAvailableThemes(excludedThemes: [String]) -> [String] {
     // Get the list of all available ZSH themes.
     guard
       let allThemes = try? FileManager.default.contentsOfDirectory(
-        atPath: Self.expandTilde(in: Zshift.themesDir))
+        atPath: ZShift.expandTilde(in: ZShift.themesDir))
     else {
-      fatalError("Failed to list themes at \(Zshift.themesDir)")
+      fatalError("Failed to list themes at \(ZShift.themesDir)")
     }
     // Filter out the bad themes.
-    return allThemes.filter {
-      !excludedThemes.contains($0.replacingOccurrences(of: ".zsh-theme", with: ""))
-    }
+    return ["random"]
+      + allThemes.filter {
+        !excludedThemes.contains($0.replacingOccurrences(of: ".zsh-theme", with: ""))
+      }
   }
 
   /// Randomly select a theme from the list of available ones
@@ -229,13 +145,54 @@ struct Like: AsyncParsableCommand {
   }
 
   /// Print out the path to the selected theme in zsh-compatible format
-  static func printSelectedTheme(_ theme: String?) {
-    if let selectedTheme = theme {
-      print("ZSH_THEME=\(selectedTheme)")
-    } else {
-      print("Error: No themes available")
-    }
+  static func printSelectedTheme(_ theme: String) {
+    Figlet.say("ZShift - " + theme)
+    print("ZSH_THEME=\(theme)")
   }
+
+  /// Read input
+  static func readInput() -> String {
+    print(
+      "Enter the path to your bad themes file (e.g., ~/excluded_zsh_themes.txt): ", terminator: "")
+    guard let excludedThemesPath = readLine() else {
+      fatalError("Failed to read file path")
+    }
+    return excludedThemesPath
+  }
+
+  func run() async throws {
+    /// For the script version just load the default
+    var excludedThemesPath = ""
+
+    // Maybe add as a resource?
+    if excludedThemesPath.count == 0 {
+      excludedThemesPath = ZShift.defaultExcludedFile
+    }
+
+    // Load excluded themes.
+    let likedThemes: [String] = ZShift.loadExcludedThemes(from: ZShift.defaultLikedFile)
+    // Load liked themes.
+    let excludedThemes: [String] = ZShift.loadExcludedThemes(from: excludedThemesPath)
+    // Filter out the seen themes.
+    var goodThemes: [String] = Self.getAvailableThemes(excludedThemes: excludedThemes + likedThemes)
+    if goodThemes.count == 0 {
+      goodThemes = likedThemes
+    }
+    // Choose a random good theme.
+    guard let randomTheme = Self.getRandomTheme(from: goodThemes) else {
+      fatalError("Random theme not there.")
+    }
+
+    Self.printSelectedTheme(randomTheme)
+  }
+}
+
+struct Like: AsyncParsableCommand {
+  static var configuration = CommandConfiguration(
+    abstract: "Like a zsh theme", helpNames: .shortAndLong)
+
+  @Argument(help: "The theme to like.")
+  var likedTheme: String
 
   /// Read input
   static func readInput() -> String {
@@ -249,7 +206,7 @@ struct Like: AsyncParsableCommand {
 
   mutating func run() async throws {
     // Load excluded themes.
-    let likedThemes = Self.loadLikedThemes(from: Zshift.defaultLikedFile)
+    let likedThemes = ZShift.loadLikedThemes(from: ZShift.defaultLikedFile)
 
     // Check if the theme is already liked
     guard !likedThemes.contains(likedTheme) else {
@@ -258,12 +215,38 @@ struct Like: AsyncParsableCommand {
     }
     // Append the new liked theme
     do {
-      try Self.appendLikedTheme(likedTheme, to: Zshift.defaultLikedFile)
+      try ZShift.append(theme: likedTheme, to: ZShift.defaultLikedFile)
     } catch {
       fatalError("Could not save '\(likedTheme)'.")
     }
 
     print("Theme '\(likedTheme)' has been added to your liked themes.")
+  }
+}
 
+struct Exclude: AsyncParsableCommand {
+  static var configuration = CommandConfiguration(
+    abstract: "Exclude a zsh theme", helpNames: .shortAndLong)
+
+  @Argument(help: "The theme to exclude.")
+  var excludeTheme: String
+
+  mutating func run() async throws {
+    // Load excluded themes.
+    let excludedThemes = ZShift.loadExcludedThemes(from: ZShift.defaultLikedFile)
+
+    // Check if the theme is already liked
+    guard !excludedThemes.contains(excludeTheme) else {
+      print("Theme '\(excludeTheme)' is already in your excluded themes.")
+      return
+    }
+    // Append the new excluded theme
+    do {
+      try ZShift.append(theme: excludeTheme, to: ZShift.defaultExcludedFile)
+    } catch {
+      fatalError("Could not exclude '\(excludeTheme)'.")
+    }
+
+    print("Theme '\(excludeTheme)' has been added to your excluded themes.")
   }
 }
