7248be60d1522ea39c9b678ab1503188a0e93209
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-08-07 13:42:29 -0700

    Update Package.swift

--- zshift/Package.swift
+++ zshift/Package.swift
@@ -3,15 +3,26 @@ import PackageDescription
 
 let package = Package(
   name: "zshift",
-  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+  platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v12), .watchOS(.v10), .macCatalyst(.v14)],
+    products: [
+    .executable(name: "zshift", targets: ["Zshift"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/example-package-figlet.git", branch: "main")
+  ],
   targets: [
     .executableTarget(
-      name: "zshift",
+      name: "Zshift",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Figlet", package: "example-package-figlet")
+      ],
       resources: [
         .copy("Resources"), // This will copy all files in the Resources folder),
       ]),
     .testTarget(
       name: "zshiftTests",
-      dependencies: ["zshift"]),
+      dependencies: ["Zshift"]),
   ]
 )
