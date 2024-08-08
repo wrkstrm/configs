c88fef5a90d10cbda2a5c4d4fa4ea6f6be8278b4
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-08-08 00:01:05 -0700

    Update Package.swift

--- zshift/Package.swift
+++ zshift/Package.swift
@@ -1,15 +1,23 @@
 // swift-tools-version: 5.10
 import PackageDescription
+import Foundation
+
+ConfigurationService.local.dependencies = [
+  .package(name: "Figlet", path: "../../clis/Figlet")
+]
+
+ConfigurationService.remote.dependencies = [
+  .package(url: "https://github.com/wrkstrm/Figlet.git", branch: "main"),
+]
 
 let package = Package(
   name: "zshift",
   platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v12), .watchOS(.v10), .macCatalyst(.v14)],
-    products: [
+  products: [
     .executable(name: "zshift", targets: ["Zshift"])
   ],
-  dependencies: [
+  dependencies: ConfigurationService.inject.dependencies + [
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
-    .package(path: "../../clis/Figlet")
   ],
   targets: [
     .executableTarget(
@@ -26,3 +34,37 @@ let package = Package(
       dependencies: ["Zshift"]),
   ]
 )
+
+// MARK: - Configuration Service
+
+public struct ConfigurationService {
+  public static let version = "0.0.0"
+
+  public var swiftSettings: [SwiftSetting] = []
+  var dependencies: [PackageDescription.Package.Dependency] = []
+
+  public static let inject: ConfigurationService = ProcessInfo.useLocalDeps ? .local : .remote
+
+  static var local: ConfigurationService = .init(swiftSettings: [.localSwiftSettings])
+
+  static var remote: ConfigurationService = .init()
+}
+
+// MARK: - PackageDescription extensions
+
+extension SwiftSetting {
+  public static let localSwiftSettings: SwiftSetting = .unsafeFlags([
+    "-Xfrontend",
+    "-warn-long-expression-type-checking=10",
+  ])
+}
+
+// MARK: - Foundation extensions
+
+extension ProcessInfo {
+  public static var useLocalDeps: Bool {
+    ProcessInfo.processInfo.environment["SPM_USE_LOCAL_DEPS"] == "true"
+  }
+}
+
+// CONFIG_SERVICE_END_V1_HASH:{{CONFIG_HASH}}
