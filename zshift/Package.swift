31a21c8d3d23912f973380af5ce80d04057e3434
Cristian A Monterroza <1240948+rismay@users.noreply.github.com>
2024-10-27 15:30:31 -0700

    Update Package.swift

--- zshift/Package.swift
+++ zshift/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     .executable(name: "zshift", targets: ["Zshift"])
   ],
   dependencies: ConfigurationService.inject.dependencies + [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0")
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
   ],
   targets: [
     .executableTarget(
